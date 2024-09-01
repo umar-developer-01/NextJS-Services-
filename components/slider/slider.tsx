c1db5b65cc3c9f22c36129a55acf052444f0556c
umar-developer-01 <umarkhandev9@gmail.com>
2024-09-01 19:08:25 +0530

    Added slider

--- components/slider/slider.tsx
+++ components/slider/slider.tsx
@@ -2,8 +2,8 @@
 import { useState } from "react";
 import { StaticImageData } from "next/image";
 import Image from "next/image";
-import { ArrowBigLeft , ArrowBigRight } from "lucide-react";
-import slidesrStyles from "./slider.module.css";
+import { ArrowBigLeft, ArrowBigRight, CircleDot,Circle } from "lucide-react";
+import sliderStyles from "./slider.module.css";
 
 interface Slide {
   url: string | StaticImageData;
@@ -17,18 +17,58 @@ interface Slides {
 export default function ImageSlider({ slides }: Slides) {
   const [imageIndex, setimageIndex] = useState<number>(0);
 
+  const showPrevImage = () => {
+    const isFirstSlide = imageIndex === 0;
+    const newSlide = isFirstSlide ? slides.length - 1 : imageIndex - 1;
+    setimageIndex(newSlide);
+  };
 
+  const showNextImage = () => {
+    const isLastSlide = imageIndex === slides.length - 1;
+    const newSlide = isLastSlide ? 0 : imageIndex + 1;
+    setimageIndex(newSlide);
+  };
 
+  console.log("this is the image index", imageIndex);
   return (
     <>
-    
-      <Image src={slides[0].url} alt={"Image"} className={slidesrStyles.imageSize} />
-      <button className={`${slidesrStyles.sliderButton} ${slidesrStyles.left}`}>
-        <ArrowBigLeft/>
+      <div className={sliderStyles.container}>
+        {slides.map((ele, index) => {
+          return (
+            <Image
+              src={ele.url}
+              key={index}
+              alt={"slider"}
+              className={sliderStyles.imageSize}
+              style={{translate:`${-100 * imageIndex}%`}}
+            />
+          );
+        })}
+      </div>
+      <button
+        className={`${sliderStyles.sliderButton} ${sliderStyles.left}`}
+        onClick={showPrevImage}
+      >
+        <ArrowBigLeft />
       </button>
-      <button className={`${slidesrStyles.sliderButton} ${slidesrStyles.right}`}>
-        <ArrowBigRight/>
+      <button
+        className={`${sliderStyles.sliderButton} ${sliderStyles.right}`}
+        onClick={showNextImage}
+      >
+        <ArrowBigRight />
       </button>
+      <div className={sliderStyles.dots}>
+        {
+          slides.map((_,index)=>{
+            return (
+              <>
+              <button onClick={()=>setimageIndex(index)} key={index} className={sliderStyles.dotButton}>
+                {index=== imageIndex? <CircleDot/>:<Circle/>}</button>
+              </>
+            )
+          })
+        }
+      </div>
     </>
   );
 }
